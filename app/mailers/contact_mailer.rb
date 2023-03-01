2914b7b52695aa3e2ea61ca5670906bae3d80659
下津浦勝 <shimotsuurasho@shitatsuuramasarunoMacBook-Air.local>
2023-03-01 23:08:23 +0900

    init

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(picture)
     @picture = picture
 
-    mail to: @picture.user, subject: "画像投稿完了メール"
+    mail to: @picture.user.email, subject: "画像投稿完了メール"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :favorites
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
